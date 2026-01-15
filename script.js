0c2dd5641203fec46a129b34f3149b7b9cbbaef0
oussama <ou2121>
2026-01-15 22:36:34 +0100

    add resizing button to the grid

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const main =document.querySelector("div")
-for(i=0;i<16;i++){
-    for(j=0;j<16;j++){
-    const container = document.createElement("div");
-   
-    container.style.cssText="box-sizing:border-box;height:calc(100%/16);width:calc(100%/16);border:  2px solid black";
-
+let size=16;
+function createGrid(size){
+for(i=0;i<size;i++){
+    for(j=0;j<size;j++){
+    let container = document.createElement("div");
+    container.style.cssText=`box-sizing:border-box;height:calc(100%/${size});width:calc(100%/${size});border:  2px solid black`;
     main.appendChild(container);
+    container.addEventListener('mouseover',()=>{
+    container.style.backgroundColor="black";
+ })
     }
-}
+}}
+createGrid(size);
+const btn =document.createElement("button");
+btn.textContent="new grid";
+document.body.appendChild(btn);
+
+btn.addEventListener("click",()=>{
+let s=prompt("enter your new grid size");
+main.innerHTML="";
+createGrid(s);
+})
